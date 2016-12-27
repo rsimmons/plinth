c880009679738186489092aa913014fe5235b10b
Russel Simmons <symstym@mac.com>
2016-12-27 14:09:55 -0800

    Reorg again and build to docs dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  entry: "./entry.js",
+  entry: "./src/entry.js",
   output: {
     path: __dirname,
-    filename: "bundle.js",
+    filename: "docs/bundle.js",
   },
   module: {
     loaders: [
