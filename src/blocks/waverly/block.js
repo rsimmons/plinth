a2e854abc6782331598f80e42e578dd58ff6aadf
Russel Simmons <symstym@mac.com>
2017-01-03 18:34:26 -0800

    Waverly: Save/load settings

--- src/blocks/waverly/block.js
+++ src/blocks/waverly/block.js
@@ -1,7 +1,7 @@
 const template = require('!raw!./template.html');
 
 export default class Waverly {
-  constructor(document, audioContext) {
+  constructor(document, audioContext, settings) {
     const INIT_WAVEFORM = 'sine';
     const INIT_FREQUENCY = 440;
     const INIT_FM_SCALE = 100.0;
@@ -15,6 +15,13 @@ export default class Waverly {
     fmScalerNode.gain.value = INIT_FM_SCALE;
     fmScalerNode.connect(oscNode.frequency);
 
+    if (settings) {
+      const settingsObj = JSON.parse(settings);
+      oscNode.type = settingsObj.waveform;
+      oscNode.frequency.value = settingsObj.frequency;
+      fmScalerNode.gain.value = settingsObj.fmScale;
+    }
+
     this.inputs = {
       // 'pitch': {type: 'audio', node: undefined}, // TODO: need a way to convert linear to exponential to make this work
       'fm': {type: 'audio', node: fmScalerNode},
@@ -27,13 +34,13 @@ export default class Waverly {
     this.panelView = tmpElem.childNodes[0];
 
     const waveformSelectElem = this.panelView.querySelector('.waveform-select');
-    waveformSelectElem.value = INIT_WAVEFORM;
+    waveformSelectElem.value = oscNode.type;
     waveformSelectElem.addEventListener('input', () => {
       oscNode.type = waveformSelectElem.value;
     });
 
     const frequencyInputElem = this.panelView.querySelector('.frequency-input');
-    frequencyInputElem.value = INIT_FREQUENCY;
+    frequencyInputElem.value = oscNode.frequency.value;
     frequencyInputElem.addEventListener('input', () => {
       const v = parseInt(frequencyInputElem.value, 10);
       if (v > 0) {
@@ -42,11 +49,19 @@ export default class Waverly {
     });
 
     const fmScaleInputElem = this.panelView.querySelector('.fm-scale-input');
-    fmScaleInputElem.value = INIT_FM_SCALE;
+    fmScaleInputElem.value = fmScalerNode.gain.value;
     fmScaleInputElem.addEventListener('input', () => {
       const v = parseInt(fmScaleInputElem.value, 10);
       fmScalerNode.gain.value = v;
     });
+
+    this.save = () => {
+      return JSON.stringify({
+        waveform: oscNode.type,
+        frequency: oscNode.frequency.value,
+        fmScale: fmScalerNode.gain.value,
+      });
+    };
   }
 }
 
