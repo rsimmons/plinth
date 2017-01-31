12206e2849aaa06541ddab9cd66a98b370ad5184
Russel Simmons <symstym@mac.com>
2017-01-31 15:45:02 -0800

    Thumper: Add icon, bg color

--- src/blocks/thumper/block.js
+++ src/blocks/thumper/block.js
@@ -1,3 +1,6 @@
+// Drum icon by Raz Cohen, CC BY 3.0 US, from https://thenounproject.com/term/bass-drum/118259/
+const DRUM_SVG_URL = require('svg-url!./drum.svg');
+
 export default class Thumper {
   constructor(document, audioContext) {
     const PITCH_LOW = 1;
@@ -27,7 +30,7 @@ export default class Thumper {
     };
 
     const tmpElem = document.createElement('div');
-    tmpElem.innerHTML = '<div style="box-sizing: border-box; width: 126px; height: 256px; padding: 5px; background-color: white;">Thumper</div>';
+    tmpElem.innerHTML = '<div style="box-sizing: border-box; width: 126px; height: 256px; padding: 5px; background-color: #e8a35f;position:relative"><div style="text-align:center;position:absolute;width:116px;bottom:0px"><img width="100" height="100" src=' + DRUM_SVG_URL + '></div></div>';
     this.panelView = tmpElem.childNodes[0];
   }
 }
