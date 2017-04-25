e26b232b6af2ef65574a69e475ead0d5c254e87e
Russel Simmons <symstym@mac.com>
2017-04-25 12:10:32 -0700

    Orinami: Reactify

--- src/blocks/orinami/block.js
+++ src/blocks/orinami/block.js
@@ -1,34 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import BlockRoot from '../../components/BlockRoot';
+import Knob from '../../components/Knob';
+
+class View extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {stages, setStages} = this.props;
+
+    return (
+      <BlockRoot widthUnits={1} extraStyles={{position: 'relative', padding: '10px', background: '#b2d9d8', fontSize: 14}}>
+        <div style={{position: 'absolute', top: 128, left: 31}}><Knob label="Stages" width={58} value={stages} onChange={setStages} integral={true} min={0} max={8} /></div>
+      </BlockRoot>
+    );
+  }
+}
+
 export default class Orinami {
   constructor(audioContext, viewContainer, settings) {
-    const MIN_STAGES = 0;
-    const INIT_STAGES = 4;
-
-    let inputGainLow;
-
     const inputGainNode = audioContext.createGain();
     inputGainNode.gain.value = 1.0;
     const shaperNode = audioContext.createWaveShaper();
     shaperNode.oversample = 'none';
-
     inputGainNode.connect(shaperNode);
-
-    const setNumStages = (stages) => {
-      const points = 2*(stages + 1);
-      const curve = new Float32Array(points);
-      for (let i = 0; i < points; i++) {
-        curve[i] = ((stages + i) % 2) ? 1 : -1;
-      }
-      shaperNode.curve = curve;
-      inputGainLow = 1.0/(points-1);
-      // TODO: get inputGainNode gain based on inputGainLow and current Amount setting
-      this.stages = stages;
-    };
-
-    if (settings) {
-      setNumStages(settings.st);
-    } else {
-      setNumStages(INIT_STAGES);
-    }
 
     this.inputs = {
       'audio': {type: 'audio', node: inputGainNode}
@@ -37,22 +34,53 @@ export default class Orinami {
       'audio': {type: 'audio', node: shaperNode},
     };
 
-    viewContainer.innerHTML = '<div style="box-sizing: border-box; width: 126px; height: 256px; padding: 5px; background-color: white;"><div>Orinami</div><div><label>Stages <input type="number" value="' + this.stages + '" min="' + MIN_STAGES + '" step="1" style="width: 50px" /></label></div></div>';
+    let stages;
+    let inputGainLow;
+    let renderReady = false;
 
-    viewContainer.querySelector('input').addEventListener('input', (e) => {
-      const st = parseInt(e.target.value, 10);
-      if (!isNaN(st)) {
-        const fst = Math.floor(st);
-        if (fst >= MIN_STAGES) {
-          setNumStages(fst);
-        }
+    const setStages = (v) => {
+      if (isNaN(v) || (v < 0)) {
+        return;
       }
-    }, false);
-  }
+      stages = v;
 
-  save() {
-    return {
-      st: this.stages,
+      const points = 2*(stages + 1);
+      const curve = new Float32Array(points);
+      for (let i = 0; i < points; i++) {
+        curve[i] = ((stages + i) % 2) ? 1 : -1;
+      }
+      shaperNode.curve = curve;
+      inputGainLow = 1.0/(points-1);
+      // TODO: get inputGainNode gain based on inputGainLow and current Amount setting
+
+      render();
+    };
+
+    const render = () => {
+      if (renderReady) {
+        ReactDOM.render(<View stages={stages} setStages={setStages} />, viewContainer);
+      }
+    }
+
+    if (!settings) {
+      settings = {
+        st: 4,
+      };
+    }
+
+    setStages(settings.st);
+
+    renderReady = true;
+    render();
+
+    this.save = () => {
+      return {
+        st: stages,
+      };
+    }
+
+    this.destroy = () => {
+      ReactDOM.unmountComponentAtNode(viewContainer);
     };
   }
 }
