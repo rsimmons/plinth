191381b3765b9277872501afbf03fd5c2dec1bc8
Russel Simmons <symstym@mac.com>
2017-04-25 16:53:27 -0700

    GridSelector: Make label optional

--- src/components/GridSelector.js
+++ src/components/GridSelector.js
@@ -17,12 +17,12 @@ export default class GridSelector extends React.Component {
 
     return (
       <div>
-        <label>{label}</label>
-        <div style={{marginTop: 2}}>
+        { label ? <label style={{marginBottom: 2}}>{label}</label> : '' }
+        <div>
           {options.map(o => {
             const active = o.value === value;
             return (
-              <button key={o.value} style={{background: active ? color : bgColor, color: active ? bgColor : color, border: '1px solid ' + color, marginTop: -1, marginRight: -1, width: cellWidth, height: cellHeight, outline: 'none', padding: 0}} data-value={o.value} onClick={this.handleClickOrTouchStart} onTouchStart={this.handleClickOrTouchStart}>{o.label}</button>
+              <button key={o.value} style={{background: active ? color : bgColor, color: active ? bgColor : color, border: '1px solid ' + color, marginTop: -1, marginRight: -1, width: cellWidth, height: cellHeight, outline: 'none', padding: 0, lineHeight: 1}} data-value={o.value} onClick={this.handleClickOrTouchStart} onTouchStart={this.handleClickOrTouchStart}>{o.label}</button>
             );
           })}
         </div>
