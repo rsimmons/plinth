75330dc7d06a5c24c3f612f0ac8606f05367fb06
Russel Simmons <symstym@mac.com>
2017-04-28 12:31:28 -0700

    Wavefolder: Add Smooth control

--- src/blocks/wavefolder/block.js
+++ src/blocks/wavefolder/block.js
@@ -9,11 +9,12 @@ class View extends React.Component {
   }
 
   render() {
-    const {stages, setStages} = this.props;
+    const {stages, setStages, smoothness, setSmoothness} = this.props;
 
     return (
       <BlockRoot widthUnits={1} extraStyles={{position: 'relative', padding: '10px', background: '#b2d9d8', fontSize: 14}}>
-        <div style={{position: 'absolute', top: 128, left: 31}}><Knob label="Stages" width={58} value={stages} onChange={setStages} integral={true} min={0} max={8} /></div>
+        <div style={{position: 'absolute', top: 78, left: 31}}><Knob label="Stages" width={58} value={stages} onChange={setStages} integral={true} min={0} max={8} /></div>
+        <div style={{position: 'absolute', top: 178, left: 31}}><Knob label="Smooth" width={58} value={smoothness} onChange={setSmoothness} integral={true} min={0} max={6} /></div>
       </BlockRoot>
     );
   }
@@ -34,9 +35,23 @@ export default class Wavefolder {
       'audio': {type: 'audio', node: shaperNode},
     };
 
-    let stages;
+    let stages = 0;
+    let smoothness = 0;
     let inputGainLow;
     let renderReady = false;
+
+    const recomputeCurve = () => {
+      const points = Math.pow(2, smoothness+1)*(2*stages + 1) + 1
+      const curve = new Float32Array(points);
+      for (let i = 0; i < points; i++) {
+        const x = (2*stages + 1)*Math.PI*(i/(points-1) - 0.5);
+        curve[i] = Math.sin(x);
+      }
+
+      shaperNode.curve = curve;
+      inputGainLow = 1.0/(points-1);
+      // TODO: get inputGainNode gain based on inputGainLow and current Amount setting
+    };
 
     const setStages = (v) => {
       if (isNaN(v) || (v < 0)) {
@@ -44,31 +59,42 @@ export default class Wavefolder {
       }
       stages = v;
 
-      const points = 2*(stages + 1);
-      const curve = new Float32Array(points);
-      for (let i = 0; i < points; i++) {
-        curve[i] = ((stages + i) % 2) ? 1 : -1;
+      recomputeCurve();
+
+      render();
+    };
+
+    const setSmoothness = (v) => {
+      if (isNaN(v) || (v < 0)) {
+        return;
       }
-      shaperNode.curve = curve;
-      inputGainLow = 1.0/(points-1);
-      // TODO: get inputGainNode gain based on inputGainLow and current Amount setting
+      smoothness = v;
+
+      recomputeCurve();
 
       render();
     };
 
     const render = () => {
       if (renderReady) {
-        ReactDOM.render(<View stages={stages} setStages={setStages} />, viewContainer);
+        ReactDOM.render(<View stages={stages} setStages={setStages} smoothness={smoothness} setSmoothness={setSmoothness} />, viewContainer);
       }
     }
 
     if (!settings) {
       settings = {
         st: 4,
+        sm: 6,
       };
+    } else {
+      // Backwards compat
+      if (settings.sm === undefined) {
+        settings.sm = 0;
+      }
     }
 
     setStages(settings.st);
+    setSmoothness(settings.sm);
 
     renderReady = true;
     render();
@@ -76,6 +102,7 @@ export default class Wavefolder {
     this.save = () => {
       return {
         st: stages,
+        sm: smoothness,
       };
     }
 
@@ -89,8 +116,13 @@ Wavefolder.blockName = 'Wavefolder';
 Wavefolder.helpText =
 `Wavefolder stretches and folds input waveforms in the amplitude dimension, introducing extremely non-linear distortion.
 
-Wavefolder first increases the amplitude of its input audio, and then any parts of the resulting waveform that exceed certain limits (the range -1 to 1) are "folded" back over those limits. In other words, when the waveform hits the limit, its direction is reversed to keep it within the range -1 to 1. A waveform visualizer (such as Scope) is useful to understand what's going on.
+Wavefolder first increases the amplitude of its input audio, and then any parts of the resulting waveform that exceed certain limits (the range -1 to 1) are "folded" back over those limits. In other words, when the waveform hits the limit, its direction is reversed to keep it within the range -1 to 1. Depending on the setting of the Smooth parameter, this fold maybe be hard/immediate) or soft/rounded. A waveform visualizer (such as Scope) is useful to understand what's going on.
 
 If an input waveform has a low amplitude, it may not hit the thresholds for folding, and just have its amplitude increased.
 
-Wavefolder has a control to set the number of "stages". Increasing numbers of stages cause more stretching and folding, and hence more distortion.`;
+The Stages control determines the maximum amount of stretching/folding (and hence distortion) that can occur.
+
+The Smooth control determines whether folding is smoothed out or not. Smooth folding will produce less harmonics and aliasing artifacts.
+
+It's recommended to use low-harmonic waveforms (sine and triangle) as inputs.
+`;
